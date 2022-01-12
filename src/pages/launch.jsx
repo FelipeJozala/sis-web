208958a60f5995fe8b2761ed87d0e61ddfa53f55
Felipe Jozala <felipe.jozala@gmail.com>
2022-01-12 08:49:54 -0300

    correction for last commit

---
+++ src/pages/launch.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const launch = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default launch
---
+++ src/pages/starship.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const starship = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default starship
